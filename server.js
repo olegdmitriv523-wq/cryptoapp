ea8abf85ef15e0584be79cc64fff2eaeb7b0a516
yourname <youremail@gmail.com>
2026-07-01 15:02:17 +0300

    Bind server to all interfaces

--- server.js
+++ server.js
@@ -2005,4 +2005,4 @@ app.post("/admin/delete-user", adminLimiter, adminAuth, async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on ${PORT}`));
